475d31b63ae77e409bd42ea6fb1a04b589730ea3
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-07-19 13:37:57 -0700

    create/post route works

--- routes/urls.js
+++ routes/urls.js
@@ -12,6 +12,10 @@ router.get('/', async (req, res) => {
     }
   })
 
+// Getting One
+router.get('/:id', getUrl, (req, res) => {
+    res.json(res.url)
+  })
 
   async function getUrl(req, res, next) {
     let url
@@ -27,5 +31,49 @@ router.get('/', async (req, res) => {
     res.url = url
     next()
   }
+
+
+// Creating one
+router.post('/', async (req, res) => {
+    let newShort = [];
+    // iterate 2x because 2x3 =6
+    for (let i =0; i < 2; i++) {
+      // lower case
+      newShort.push(String.fromCharCode(Math.floor(Math.random() *26) + 97));
+      // upper case
+      newShort.push(String.fromCharCode(Math.floor(Math.random() *26) + 65));
+      // numbers
+      newShort.push(String.fromCharCode(Math.floor(Math.random() *10)+ 48));
+    };
+  
+    shuffledShort = shuffleArray(newShort);
+    shortString =shuffledShort.join('');
+    console.log(req.body.original + " is now: " + "https://" + shortString);
+  
+    const url = new Url({
+      original: req.body.original,
+      short: shortString,
+      count: 1
+    })
+    try {
+      const newUrl = await url.save()
+      res.status(201).json(newUrl)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  })
+
+// standard shuffle array
+  function shuffleArray(array) {
+  
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+   return array;
+  }
+  
   
   module.exports = router
